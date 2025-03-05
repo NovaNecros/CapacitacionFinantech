718d442c35f776255c4919195aaa99aca544273d
NovaNecros <uchihaespurr@gmail.com>
2025-03-05 14:55:34 -0600

    Sigo sin poder cambiar el color del botón cuando hago una forma drawable.

--- app/src/main/java/com/capacitacion/drawableresources/MainActivity.java
+++ app/src/main/java/com/capacitacion/drawableresources/MainActivity.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.graphics.drawable.Drawable;
 
 public class MainActivity extends AppCompatActivity
 {
-    Drawable imagenGato;
+    Button botonHexagonal;
+    Button botonReinicio;
+    ImageView imagenGato;
+    Drawable drawableGato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,7 +28,34 @@ public class MainActivity extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        imagenGato = getResources().getDrawable(R.drawable.crazycafinecat);
+        botonHexagonal = (Button) findViewById(R.id.botonHexagonal);
+        botonReinicio = (Button) findViewById(R.id.botonReinicio);
+        imagenGato = (ImageView) findViewById(R.id.imagenGato);
+        drawableGato = ContextCompat.getDrawable(this, R.drawable.crazycafinecat);
+
+        botonHexagonal.setVisibility(View.VISIBLE);
+        botonReinicio.setVisibility(View.GONE);
+        imagenGato.setVisibility(View.GONE);
+
+        botonHexagonal.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                botonHexagonal.setVisibility(View.GONE);
+                botonReinicio.setVisibility(View.VISIBLE);
+                imagenGato.setVisibility(View.VISIBLE);
+            }
+        });
+
+        botonReinicio.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                recreate();
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
         {

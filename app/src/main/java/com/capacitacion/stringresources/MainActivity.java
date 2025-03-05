5f6137a7f83f0c66d8db6369d4c530f0032d1fab
NovaNecros <uchihaespurr@gmail.com>
2025-03-05 11:37:18 -0600

    Usando string resources para el texto de los botones y los textviews.

--- app/src/main/java/com/capacitacion/stringresources/MainActivity.java
+++ app/src/main/java/com/capacitacion/stringresources/MainActivity.java
@@ -8,9 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity
 {
-    String hola;
+    TextView textoArriba;
+    TextView textoAbajo;
+    Button botonInicio;
+    Button botonReinicio;
+    ImageView imagenSaludo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +30,45 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        textoArriba = (TextView) findViewById(R.id.textoArriba);
+        textoAbajo = (TextView) findViewById(R.id.textoAbajo);
+        botonInicio = (Button) findViewById(R.id.botonInicio);
+        botonReinicio = (Button) findViewById(R.id.botonReinicio);
+        imagenSaludo = (ImageView) findViewById(R.id.imagenSaludo);
+
+        textoArriba.setText(R.string.instructions);
+        textoAbajo.setText(R.string.meme);
+
+        textoArriba.setVisibility(View.GONE);
+        textoAbajo.setVisibility(View.GONE);
+        botonInicio.setVisibility(View.VISIBLE);
+        botonReinicio.setVisibility(View.GONE);
+        imagenSaludo.setVisibility(View.VISIBLE);
+
+        botonInicio.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                textoArriba.setVisibility(View.VISIBLE);
+                textoAbajo.setVisibility(View.VISIBLE);
+                botonInicio.setVisibility(View.GONE);
+                botonReinicio.setVisibility(View.VISIBLE);
+                imagenSaludo.setVisibility(View.GONE);
+            }
+        });
+
+        botonReinicio.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                recreate();
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
         {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());

db29d6d74a60bee54ebf4d7faaa535d7911f447e
NovaNecros <uchihaespurr@gmail.com>
2025-03-05 12:52:50 -0600

    Primer acercamiento al uso de drawable resources.

---
+++ app/src/main/java/com/capacitacion/drawableresources/MainActivity.java
@@ -0,0 +1,33 @@
+package com.capacitacion.drawableresources;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.graphics.drawable.Drawable;
+
+public class MainActivity extends AppCompatActivity
+{
+    Drawable imagenGato;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        imagenGato = getResources().getDrawable(R.drawable.crazycafinecat);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

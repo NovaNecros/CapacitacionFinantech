73b45551c21ca7b0483f35d9ac5ee4143eeef76f
NovaNecros <uchihaespurr@gmail.com>
2025-03-07 12:29:51 -0600

    Toasts personalizados.

--- app/src/main/java/com/capacitacion/listeners/MainActivity.java
+++ app/src/main/java/com/capacitacion/listeners/MainActivity.java
@@ -12,13 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.Gravity;
+import androidx.core.content.ContextCompat;
+import android.graphics.drawable.GradientDrawable;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    TextView titulo;
-    Button boton1;
-    Button boton2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,9 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        titulo = (TextView) findViewById(R.id.titulo);
-        boton1 = (Button) findViewById(R.id.boton1);
-        boton2 = (Button) findViewById(R.id.boton2);
+        TextView titulo = (TextView) findViewById(R.id.titulo);
+        Button boton1 = (Button) findViewById(R.id.boton1);
+        Button boton2 = (Button) findViewById(R.id.boton2);
 
         boton1.setOnClickListener(this);
         boton2.setOnClickListener(this);
@@ -45,14 +47,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view)
     {
         int id = view.getId();
+        String texto;
+        int color;
 
         if(id == R.id.boton1)
         {
-            Toast.makeText(getApplicationContext(), "picaste boton 1", Toast.LENGTH_SHORT).show();
+            texto = getString(R.string.textotoast1);
+            color = ContextCompat.getColor(this, R.color.azulchillon);
+            showToast(texto, color);
         }
         else if(id == R.id.boton2)
         {
-            Toast.makeText(getApplicationContext(), "picaste boton 2", Toast.LENGTH_SHORT).show();
+            texto = getString(R.string.textotoast2);
+            color = ContextCompat.getColor(this, R.color.rojochillon);
+            showToast(texto, color);
         }
+    }
+
+    private void showToast(String texto, int color)
+    {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, findViewById(R.id.main), false);
+        Toast toast = new Toast(getApplicationContext());
+        TextView contenido = layout.findViewById(R.id.textoToast);
+        GradientDrawable burbuja = new GradientDrawable();
+
+        burbuja.setColor(color);
+        burbuja.setCornerRadius(25);
+        layout.setBackground(burbuja);
+        contenido.setText(texto);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.show();
     }
 }

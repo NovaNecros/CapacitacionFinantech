5323e9a0a941f47828f60e21c3ce878b5c2a60a7
NovaNecros <uchihaespurr@gmail.com>
2025-03-11 21:34:48 -0600

    Spinner personalizado :)

---
+++ app/src/main/java/com/capacitacion/spinners/CustomSpinnerAdapter.java
@@ -0,0 +1,49 @@
+package com.capacitacion.spinners;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+public class CustomSpinnerAdapter extends ArrayAdapter<String>
+{
+    private Context context;
+    private String [] items;
+
+    public CustomSpinnerAdapter(@NonNull Context context, int resource, @NonNull String [] objects)
+    {
+        super(context, resource, objects);
+        this.context = context;
+        this.items = objects;
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View view = super.getView(position, convertView, parent);
+        setColorAlterno(position, view);
+        return view;
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View view = super.getDropDownView(position, convertView, parent);
+        setColorAlterno(position, view);
+        return view;
+    }
+
+    private void setColorAlterno(int position, View view)
+    {
+        if(view instanceof TextView)
+        {
+            TextView textView = (TextView) view;
+            int color = position == 0 ? R.color.plateado : (position % 2 == 0) ? R.color.brat : R.color.verdebosque;
+            textView.setBackgroundColor(ContextCompat.getColor(context, color));
+        }
+    }
+}
--- app/src/main/java/com/capacitacion/spinners/MainActivity.java
+++ app/src/main/java/com/capacitacion/spinners/MainActivity.java
@@ -1,5 +1,9 @@
 package com.capacitacion.spinners;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    List<String> family;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -30,8 +35,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         });
 
         Spinner familySpinner = (Spinner) findViewById(R.id.family_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.family, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        family = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.family)));
+        family.add(0, getResources().getString(R.string.spinner_default));
+
+        CustomSpinnerAdapter adapter = new CustomSpinnerAdapter(this, R.layout.spinner_item, family.toArray(new String[0]));
+        adapter.setDropDownViewResource(R.layout.dropdown_item);
 
         familySpinner.setAdapter(adapter);
         familySpinner.setOnItemSelectedListener(this);
@@ -40,9 +48,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
     {
-        Toast.makeText(getApplicationContext(), "Selected: " + (position+1), Toast.LENGTH_SHORT).show();
+        if(position != 0)
+        {
+            Toast.makeText(getApplicationContext(), "Elegiste a " + family.get(position), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), ">:(", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {}
+    public void onNothingSelected(AdapterView<?> parent)
+    {
+        Toast.makeText(getApplicationContext(), ">:(", Toast.LENGTH_SHORT).show();
+
+    }
 }

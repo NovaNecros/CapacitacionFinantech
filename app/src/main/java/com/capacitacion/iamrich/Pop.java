85d23cb16f9c1debac2808de389c4adc13063fab
NovaNecros <uchihaespurr@gmail.com>
2025-03-13 13:08:06 -0600

    Moví todo de lugar nada más.

--- app/src/main/java/com/capacitacion/iamrich/Pop.java
+++ app/src/main/java/com/capacitacion/iamrich/Pop.java
@@ -18,6 +18,6 @@ public class Pop extends Activity
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*0.8),(int)(height*0.8));
+        getWindow().setLayout((int)(width*0.6),(int)(height*0.3));
     }
 }
